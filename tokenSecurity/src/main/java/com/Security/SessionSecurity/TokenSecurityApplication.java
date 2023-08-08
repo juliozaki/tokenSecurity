9c1462eafe3075007cedceeb28dd16e7ef050a62
juliozaki <70343801+juliozaki@users.noreply.github.com>
2023-08-08 17:37:49 -0600

    Se crea el proyecto
    Se crea el proyecto

---
+++ tokenSecurity/src/main/java/com/Security/SessionSecurity/TokenSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.Security.SessionSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenSecurityApplication.class, args);
+	}
+
+}
